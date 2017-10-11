3577eaad4152fc32897e72339fb40041dd627f3b
jarvisru <dar58896123@gmail.com>
2017-10-11 22:59:54 +0800

    still need to working on rank calculate ver1 haha

--- Homework/HW1_104502012.cpp
+++ Homework/HW1_104502012.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int n;
 
-/* For Step 1 */
-// merge and just compare by value x
+/*  For Step 1
+    merge and just compare by value x */
 void Merge_x(int left, int middle, int right, int *arr_X, int *arr_Y){
 
     int temp[2][right-left+1];
@@ -76,27 +76,27 @@ void Merge_sort_x(int left, int right, int *arr_X, int *arr_Y){
     }
 }
 
-/* For Step 2 */
-// merge and compare by value y and do ranking calculate
+/*  For Step 2
+    merge and compare by value y and do ranking calculate */
 void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *x, int *y, int *r){
 
     int temp[2][right-left+1];
     // for merge easily
     int i=left,j=middle+1;
     int rear=0;
-    cout<<left<<right<<n<<endl;
+
     // compare and assign value to temp
     while(i<=middle && j<=right){
         if(arr_Y[i] > arr_Y[j]){
             temp[0][rear] = arr_X[j];
             temp[1][rear] = arr_Y[j];
-            ++rear;
+
             ++j;
         }
         else if(arr_Y[i] < arr_Y[j]){
             temp[0][rear] = arr_X[i];
             temp[1][rear] = arr_Y[i];
-            ++rear;
+
             ++i;
         }
         // same value of y => compare x
@@ -104,27 +104,32 @@ void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *x, in
             if(arr_X[i] > arr_X[j]){
                 temp[0][rear] = arr_X[j];
                 temp[1][rear] = arr_Y[j];
-                ++rear;
+
                 ++j;
             }
             else{
                 temp[0][rear] = arr_X[i];
                 temp[1][rear] = arr_Y[i];
-                ++rear;
+
                 ++i;
             }
         }
+
+        ++rear;
     }
     // for remaining value
     while(i<=middle){
         temp[0][rear] = arr_X[i];
         temp[1][rear] = arr_Y[i];
+
         ++rear;
         ++i;
+
     }
     while(j<=right){
         temp[0][rear] = arr_X[j];
         temp[1][rear] = arr_Y[j];
+
         ++rear;
         ++j;
     }
@@ -154,25 +159,29 @@ int main(){
 
         int x[n],y[n],tmp_x[n],tmp_y[n],used[n],r[n];
 
-        for(int i=0 ; i<n ; i++)
+        for(int i=0 ; i<n ; i++){
             cin>>x[i]>>y[i];
+            r[i] = 0;
+        }
 
-        // sort by value x
+
+        /* Step1: sort by value x */
         Merge_sort_x(0,n-1,x,y);
 
-        cout<<"merge by x:"<<endl;
-        for(int i=0 ; i<n ; i++)
-            cout<<x[i]<<" "<<y[i]<<endl;
         // save now to temp
         for(int i=0 ; i<n ; i++){
             tmp_x[i] = x[i];
             tmp_y[i] = y[i];
         }
-        // sort by value y and do rank calculate
+
+        /* Step2: sort by value y and do rank calculate*/
         Merge_sort_y(0,n-1,x,y,tmp_x,tmp_y,r);
         cout<<"merge by y:"<<endl;
         for(int i=0 ; i<n ; i++)
             cout<<x[i]<<" "<<y[i]<<endl;
+        cout<<endl<<"testing:"<<endl;
+        for(int i=0 ; i<n ; i++)
+            cout<<r[i]<<" ";
 
     }
     return 0;
