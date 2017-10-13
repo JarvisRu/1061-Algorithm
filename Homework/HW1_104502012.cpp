08c737cb543202f4923ab0c4daa9763677b50b87
jarvisru <dar58896123@gmail.com>
2017-10-13 19:04:48 +0800

    rank calculate is done still need to work on threshold

--- Homework/HW1_104502012.cpp
+++ Homework/HW1_104502012.cpp
@@ -7,9 +7,9 @@ int n;
 
 /*  For Step 1
     merge and just compare by value x */
-void Merge_x(int left, int middle, int right, int *arr_X, int *arr_Y){
+void Merge_x(int left, int middle, int right, int *arr_X, int *arr_Y, int *order){
 
-    int temp[2][right-left+1];
+    int temp[3][right-left+1];
     // for merge easily
     int i=left,j=middle+1;
     int rear=0;
@@ -18,12 +18,14 @@ void Merge_x(int left, int middle, int right, int *arr_X, int *arr_Y){
         if(arr_X[i] > arr_X[j]){
             temp[0][rear] = arr_X[j];
             temp[1][rear] = arr_Y[j];
+            temp[2][rear] = order[j];
             ++rear;
             ++j;
         }
         else if(arr_X[i] < arr_X[j]){
             temp[0][rear] = arr_X[i];
             temp[1][rear] = arr_Y[i];
+            temp[2][rear] = order[i];
             ++rear;
             ++i;
         }
@@ -32,12 +34,14 @@ void Merge_x(int left, int middle, int right, int *arr_X, int *arr_Y){
             if(arr_Y[i] > arr_Y[j]){
                 temp[0][rear] = arr_X[j];
                 temp[1][rear] = arr_Y[j];
+                temp[2][rear] = order[j];
                 ++rear;
                 ++j;
             }
             else{
                 temp[0][rear] = arr_X[i];
                 temp[1][rear] = arr_Y[i];
+                temp[2][rear] = order[i];
                 ++rear;
                 ++i;
             }
@@ -47,12 +51,14 @@ void Merge_x(int left, int middle, int right, int *arr_X, int *arr_Y){
     while(i<=middle){
         temp[0][rear] = arr_X[i];
         temp[1][rear] = arr_Y[i];
+        temp[2][rear] = order[i];
         ++rear;
         ++i;
     }
     while(j<=right){
         temp[0][rear] = arr_X[j];
         temp[1][rear] = arr_Y[j];
+        temp[2][rear] = order[j];
         ++rear;
         ++j;
     }
@@ -60,27 +66,28 @@ void Merge_x(int left, int middle, int right, int *arr_X, int *arr_Y){
     for(int i=left ; i<=right ; i++){
         arr_X[i] = temp[0][i-left];
         arr_Y[i] = temp[1][i-left];
+        order[i] = temp[2][i-left];
     }
 
 }
 
-void Merge_sort_x(int left, int right, int *arr_X, int *arr_Y){
+void Merge_sort_x(int left, int right, int *arr_X, int *arr_Y, int *order){
     int middle;
     if(left < right){
         middle = (left+right)/2;
         // split the data
-        Merge_sort_x(left,middle,arr_X,arr_Y);
-        Merge_sort_x(middle+1,right,arr_X,arr_Y);
+        Merge_sort_x(left,middle,arr_X,arr_Y,order);
+        Merge_sort_x(middle+1,right,arr_X,arr_Y,order);
         // do merge
-        Merge_x(left,middle,right,arr_X,arr_Y);
+        Merge_x(left,middle,right,arr_X,arr_Y,order);
     }
 }
 
 /*  For Step 2
     merge and compare by value y and do ranking calculate */
-void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *x, int *y, int *r){
+void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *order, int *r){
 
-    int temp[2][right-left+1];
+    int temp[3][right-left+1];
     // for merge easily
     int i=left,j=middle+1;
     int rear=0;
@@ -90,12 +97,15 @@ void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *x, in
         if(arr_Y[i] > arr_Y[j]){
             temp[0][rear] = arr_X[j];
             temp[1][rear] = arr_Y[j];
+            temp[2][rear] = order[j];
+            r[order[j]] += i-left;
 
             ++j;
         }
         else if(arr_Y[i] < arr_Y[j]){
             temp[0][rear] = arr_X[i];
             temp[1][rear] = arr_Y[i];
+            temp[2][rear] = order[i];
 
             ++i;
         }
@@ -104,12 +114,15 @@ void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *x, in
             if(arr_X[i] > arr_X[j]){
                 temp[0][rear] = arr_X[j];
                 temp[1][rear] = arr_Y[j];
+                temp[2][rear] = order[j];
+                r[order[j]] += i-left;
 
                 ++j;
             }
             else{
                 temp[0][rear] = arr_X[i];
                 temp[1][rear] = arr_Y[i];
+                temp[2][rear] = order[i];
 
                 ++i;
             }
@@ -121,6 +134,7 @@ void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *x, in
     while(i<=middle){
         temp[0][rear] = arr_X[i];
         temp[1][rear] = arr_Y[i];
+        temp[2][rear] = order[i];
 
         ++rear;
         ++i;
@@ -129,6 +143,8 @@ void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *x, in
     while(j<=right){
         temp[0][rear] = arr_X[j];
         temp[1][rear] = arr_Y[j];
+        temp[2][rear] = order[i];
+        r[order[j]] += i-left;
 
         ++rear;
         ++j;
@@ -137,51 +153,47 @@ void Merge_y(int left, int middle, int right, int *arr_X, int *arr_Y, int *x, in
     for(int i=left ; i<=right ; i++){
         arr_X[i] = temp[0][i-left];
         arr_Y[i] = temp[1][i-left];
+        order[i] = temp[2][i-left];
     }
 
 }
 
-void Merge_sort_y(int left, int right, int *arr_X, int *arr_Y, int *x, int *y, int *r){
+void Merge_sort_y(int left, int right, int *arr_X, int *arr_Y, int *order, int *r){
     int middle;
     if(left < right){
         middle = (left+right)/2;
         // split the data
-        Merge_sort_y(left,middle,arr_X,arr_Y,x,y,r);
-        Merge_sort_y(middle+1,right,arr_X,arr_Y,x,y,r);
+        Merge_sort_y(left,middle,arr_X,arr_Y,order,r);
+        Merge_sort_y(middle+1,right,arr_X,arr_Y,order,r);
         // do merge
-        Merge_y(left,middle,right,arr_X,arr_Y,x,y,r);
+        Merge_y(left,middle,right,arr_X,arr_Y,order,r);
     }
 }
 
 int main(){
 
     while(cin>>n){
-
-        int x[n],y[n],tmp_x[n],tmp_y[n],used[n],r[n];
+        if(n<5||n>100000){
+            cout<<"Warning : Value of n should between 5 and 100000"<<endl;
+            continue;
+        }
+        int x[n],y[n],order[n],r[n];
 
         for(int i=0 ; i<n ; i++){
             cin>>x[i]>>y[i];
             r[i] = 0;
+            order[i] = i;
         }
-
 
         /* Step1: sort by value x */
-        Merge_sort_x(0,n-1,x,y);
-
-        // save now to temp
-        for(int i=0 ; i<n ; i++){
-            tmp_x[i] = x[i];
-            tmp_y[i] = y[i];
-        }
+        Merge_sort_x(0,n-1,x,y,order);
 
         /* Step2: sort by value y and do rank calculate*/
-        Merge_sort_y(0,n-1,x,y,tmp_x,tmp_y,r);
-        cout<<"merge by y:"<<endl;
-        for(int i=0 ; i<n ; i++)
-            cout<<x[i]<<" "<<y[i]<<endl;
-        cout<<endl<<"testing:"<<endl;
+        Merge_sort_y(0,n-1,x,y,order,r);
+        cout<<"Rank: ";
         for(int i=0 ; i<n ; i++)
             cout<<r[i]<<" ";
+        cout<<endl<<"======================="<<endl;
 
     }
     return 0;
