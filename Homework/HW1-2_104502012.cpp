bd5d641e61dd28e603bdb408099a03042819f8e9
jarvisru <dar58896123@gmail.com>
2017-10-18 01:14:32 +0800

    some simple modify

--- Homework/HW1-2_104502012.cpp
+++ Homework/HW1-2_104502012.cpp
@@ -3,8 +3,8 @@
 #include<ctime>
 using namespace std;
 
-
 int n;
+int t; // threshold
 
 /*  For Step 1
     merge and just compare by value x */
@@ -163,75 +163,75 @@ void insertion_sort(int left, int right, int *arr_X, int *arr_Y, int *order, int
     for(int i=left+1 ; i<right+1 ; i++){
         int target[3] = {arr_Y[i],arr_X[i],order[i]};
         int j;
-        cout<<target[0]<<endl;
+        // cout<<target[0]<<endl;
         for(j=i ; j>left && target[0]<arr_Y[j-1] ; j--){
             arr_Y[j] = arr_Y[j-1];
             arr_X[j] = arr_X[j-1];
             order[j] = order[j-1];
         }
-        cout<<"j: "<<j<<endl;
+        // cout<<"j: "<<j<<endl;
         arr_Y[j] = target[0];
         arr_X[j] = target[1];
         order[j] = target[2];
         r[order[j]] += j-left;
-        cout<<"R["<<order[j]<<"]: "<<r[order[j]]<<endl;
+        // cout<<"R["<<order[j]<<"]: "<<r[order[j]]<<endl;
     }
 }
 
-void Merge_sort_y(int left, int right, int *arr_X, int *arr_Y, int *order, int *r){
+void Merge_sort_y(int left, int right, int *arr_X, int *arr_Y, int *order, int *r, int t){
     int middle;
-    int t=3;
     if(right-left>t){
         middle = (left+right)/2;
         // split the data
-        Merge_sort_y(left,middle,arr_X,arr_Y,order,r);
-        Merge_sort_y(middle+1,right,arr_X,arr_Y,order,r);
+        Merge_sort_y(left,middle,arr_X,arr_Y,order,r,t);
+        Merge_sort_y(middle+1,right,arr_X,arr_Y,order,r,t);
         // do merge
         Merge_y(left,middle,right,arr_X,arr_Y,order,r);
     }
     else{
-        cout<<"IN : "<<left<<" "<<right<<endl;
+        // cout<<"IN : "<<left<<" "<<right<<endl;
         insertion_sort(left,right,arr_X,arr_Y,order,r);
     }
 }
 
 int main(){
 
+    cout<<"Please input a number as n, then it will create n point with random value: ";
     while(cin>>n){
-        if(n<5||n>100000){
-            cout<<"Warning : Value of n should between 5 and 100000"<<endl;
+        if(n<=0) break;
+        else if(n<5||n>100000){
+            cout<<"Value of n should between 5 and 100000"<<endl;
+            cout<<"Please input a number as n, then it will create n point with random value: ";
             continue;
         }
         int x[n],y[n],order[n],r[n];
         srand(time(NULL));
         clock_t start,stop;
-        double duration;
-
-        // set with random number
+        long double duration;
+        // assign random number to point
         for(int i=0 ; i<n ; i++){
-            cin>>x[i]>>y[i];
-            // x[i] = rand()%100;
-            // y[i] = rand()%100;
+            x[i] = rand()%100;
+            y[i] = rand()%100;
             r[i] = 0;
             order[i] = i;
         }
-        start = clock();
-        /* Step1: sort by value x */
-        Merge_sort_x(0,n-1,x,y,order);
-
-        /* Step2: sort by value y and do rank calculate*/
-        Merge_sort_y(0,n-1,x,y,order,r);
-        stop = clock();
-
-        duration = (double)(stop-start)/CLOCKS_PER_SEC;
-
-        cout<<"Rank: ";
-        for(int i=0 ; i<n ; i++)
-            cout<<r[i]<<" ";
-
-        cout<<endl<<"Time: "<<duration<<endl;
+        cout<<"Please input your testing threshold(27-30 will be best): ";
+        while(cin>>t){
+            if(t<=0) break;
+            start = clock();
+            Merge_sort_x(0,n-1,x,y,order);
+            Merge_sort_y(0,n-1,x,y,order,r,t);
+            stop = clock();
+            duration = (long double)(stop-start)/CLOCKS_PER_SEC;
+//        cout<<"Rank: ";
+//        for(int i=0 ; i<n ; i++)
+//            cout<<r[i]<<" ";
+            cout<<"Time: "<<duration<<endl;
+            cout<<"======================="<<endl;
+            cout<<"Please input your testing threshold(27-30 will be best): ";
+        }
         cout<<"======================="<<endl;
-
+        cout<<"Please input a number as n, then it will create n point with random value: ";
     }
     return 0;
 }
