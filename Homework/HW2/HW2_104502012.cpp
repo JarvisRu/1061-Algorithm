aa0b5163009ee6ccd51e33d5bdbb74dde8a2fbf7
jarvisru <dar58896123@gmail.com>
2017-11-08 17:14:51 +0800

    add zero row and column simplify alogrithm

--- Homework/HW2/HW2_104502012.cpp
+++ Homework/HW2/HW2_104502012.cpp
@@ -3,15 +3,6 @@
 #include<string>
 #include<algorithm>
 using namespace std;
-//1
-//lRk3W1I9WxwL8u3l2GA4 3In9qxKcLuN963Yxlvq4
-//3I9xLu3l4
-//1
-//bdcaba abcbdab
-//bcab
-//1
-//ABACABAC CABCBAAB
-//ABAAB
 
 
 int* getScore(int mode, string a, string b, int m, int n){
@@ -20,46 +11,26 @@ int* getScore(int mode, string a, string b, int m, int n){
         reverse(b.begin(),b.end());
     }
 
-    int *prev = new int[n];
-    int *now = new int[n];
+    int *prev = new int[n+1];
+    int *now = new int[n+1];
 
-    for(int i=0 ; i<m ; ++i){
-        for(int j=0 ; j<n ; ++j){
-            // for first row
-            if(i==0){
-                if(a[i]==b[j])
-                    now[j] = 1;
-                else if(now[j-1]==1)
-                    now[j] = 1;
-                else
-                    now[j] = 0;
+    for(int i=0 ; i<m+1 ; ++i){
+        for(int j=0 ; j<n+1 ; ++j){
+            if(i==0 || j==0){
+                now[j] = 0;
             }
-            // for others
             else{
-                if(j==0){
-                    if(a[i]==b[j])
-                        now[j] = 1;
-                    else
-                        now[j] = prev[j];
-                }
-                else{
-                    if(a[i]==b[j])
-                        now[j] = prev[j-1] + 1;
-                    else
-                        now[j] = max(prev[j],now[j-1]);
-                }
-
+                now[j] = (a[i-1]==b[j-1])? prev[j-1]+1 : max(prev[j],now[j-1]) ;
             }
         }
         // copy now_row to previous_row
-        for(int k=0 ; k<n ; ++k){
+        for(int k=0 ; k<=n ; ++k){
             prev[k] = now[k];
         }
     }
     delete [] prev;
-    // cout<<"x: "<<a<<" / y: "<<b<<endl;
-    return now;
 
+    return now;
 }
 
 string lcs(string x, string y){
@@ -71,11 +42,9 @@ string lcs(string x, string y){
     string res;
     if(m==1 || n==1){
         if(m==1 && y.find(x[0])!=string::npos){
-            // cout<<"hit: "<<x[0]<<endl;
             return res+=x[0];
         }
         else if(n==1 && x.find(y[0])!=string::npos){
-            // cout<<"hit: "<<y[0]<<endl;
             return res+=y[0];
         }
         else return "";
@@ -87,33 +56,16 @@ string lcs(string x, string y){
         score_l = getScore(1,x.substr(0,m/2), y, m/2, n);
         score_r = getScore(2,x.substr(m/2,m-m/2), y, m-m/2, n);
 
-//        cout<<"Score_l: ";
-//        for(int i=0 ; i<n ; ++i){
-//            cout<<*(score_l+i)<<" ";
-//        }
-//        cout<<endl<<"Score_r: ";
-//        for(int i=0 ; i<n ; ++i){
-//            cout<<*(score_r+i)<<" ";
-//        }
-
-        for(int i=0 ; i<n ; ++i){
-            if(*(score_l+i)+ *(score_r+n-i-2) > max_sum && i!=n-1){
-                max_sum = *(score_l+i)+*(score_r+n-i-2);
+        // find max as k
+        for(int i=0 ; i<=n ; ++i){
+            if(*(score_l+i)+ *(score_r+n-i) > max_sum){
+                max_sum = *(score_l+i)+*(score_r+n-i);
                 k = i;
-            }
-            if(i==n-1 && *(score_l+i) > max_sum){
-                return res+=y[n-1];
             }
         }
         if(max_sum==0) return "";
 
-//        cout<<"max: "<<max_sum<<" "<<"k: "<<k<<endl;
-//        cout<<m<<" "<<n<<endl;
-//        cout<<x<<" "<<y<<endl;
-//        cout<<x.substr(0,m/2)<<" , "<<y.substr(0,k+1)<<endl;
-//        cout<<x.substr(m/2,m-m/2)<<" , "<<y.substr(k+1,n-k+1)<<endl;
-//        cout<<"============================"<<endl;
-        return lcs(x.substr(0,m/2),y.substr(0,k+1)) + lcs(x.substr(m/2,m-m/2),y.substr(k+1,n-k+1));
+        return lcs(x.substr(0,m/2),y.substr(0,k)) + lcs(x.substr(m/2,m-m/2),y.substr(k,n-k));
     }
 }
 
@@ -123,16 +75,18 @@ string lcs(string x, string y){
 int main(){
     int t;
     cin>>t;
-    // if(t<2||t>20) return 0;
+    if(t<2||t>20) return 0;
     while(t>0){
         --t;
         string x,y;
         cin>>x>>y;
 
-        // if(m<5||m>1000||n<5||n>1000) break;
-        cout<<lcs(x,y);
-        cout<<endl;
+        int m,n;
+        m = x.length();
+        n = y.length();
+        if(m<5||m>1000||n<5||n>1000) break;
 
+        cout<<lcs(x,y)<<endl;
     }
     return 0;
 }
